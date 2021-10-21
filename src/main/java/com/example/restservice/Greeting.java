ef99c3af9fcb56eb72d1624b5aae181d075a41e1
Bruno Freitas <bcfreitas@gmail.com>
2021-10-20 23:24:54 -0300

    commit inicial

---
+++ src/main/java/com/example/restservice/Greeting.java
@@ -0,0 +1,21 @@
+package com.example.restservice;
+
+public class Greeting {
+
+	private final long id;
+	private final String content;
+	
+	public Greeting(long l, String content) {
+		this.id=l;
+		this.content=content;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+}
